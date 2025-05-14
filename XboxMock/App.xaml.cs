9aa191532ef86628d72f28dcc0a8eceadb5d1d16
Beren <berencmgb@gmail.com>
2025-05-14 23:05:18 +0100

    Initial commit

---
+++ XboxMock/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace XboxMock;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
