0c7b35f7f8ddef15c8b81c16339dae8d6c45d72c
Beren <berencmgb@gmail.com>
2025-05-16 00:03:06 +0100

    Setup styles for navbar

--- XboxMock/Views/ContentPages/HomePage.xaml.cs
+++ XboxMock/Views/ContentPages/HomePage.xaml.cs
@@ -11,10 +11,48 @@ public partial class HomePage : ContentPage
 {
     private readonly HomePageViewModel _viewModel;
     
+    /// <summary>
+    /// The amount to scroll to calculate the opacity
+    /// </summary>
+    private readonly int _scrollAmount = 100;
+    
+    private readonly Color _backgroundColor = Color.FromArgb("#161716");
+
+    public Color NavbarColor
+    {
+        get => (Color)GetValue(NavbarColorProperty);
+        set => SetValue(NavbarColorProperty, value);
+    }
+    
+    public static readonly BindableProperty NavbarColorProperty = BindableProperty
+        .Create(nameof(NavbarColor),
+            typeof(Color),
+            typeof(HomePage),
+            Colors.Transparent);
+    
     public HomePage(HomePageViewModel viewModel)
     {
         _viewModel = viewModel;
         BindingContext = _viewModel;
+        
         InitializeComponent();
+        CalculateNavbarColor();
+    }
+    
+    private void ScrollView_OnScrolled(object? sender,
+        ScrolledEventArgs e)
+    {
+        CalculateNavbarColor();
+    }
+
+    private void CalculateNavbarColor()
+    {
+        if (ScrollView == null)
+        {
+            return;
+        }
+        
+        var opacity = (float)(ScrollView.ScrollY / _scrollAmount);
+        NavbarColor = new Color(_backgroundColor.Red, _backgroundColor.Green, _backgroundColor.Blue, opacity);
     }
 }
